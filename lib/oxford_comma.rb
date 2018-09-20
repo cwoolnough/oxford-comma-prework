21bcb23903b4dc384ee851b762f7162b980b3ddd
cwoolnoughchamorro <cwoolnoughchamorro@googlemail.com>
2018-09-20 09:53:02 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -3,7 +3,7 @@ def oxford_comma(array)
     return array.join
   elsif array.length < 3
     return array.join(" and ")
-  elsif array.length == 3
+  elsif array.length >= 3
     return "#{array[0...-1].join(", ")}, and #{array[-1]}"
   end
 end
